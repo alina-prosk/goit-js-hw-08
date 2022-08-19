966f630c0158c2a5fe9e095db06774460a2f93f6
Alina Proskurnitska <proskur1987@gmail.com>
2022-08-19 20:05:01 +0300

    second-task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(function getTime (e) {
+    const currentSecond = JSON.stringify(e.seconds);
+
+    localStorage.setItem(LOCALSTORAGE_KEY, currentSecond);
+}), 1000);
+    
+const timeLocalStorage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+player.setCurrentTime(timeLocalStorage);
 
