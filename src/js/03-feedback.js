dc2c525b0bb2738d2a1df089910c7e2189330a1f
Alina Proskurnitska <proskur1987@gmail.com>
2022-08-20 16:29:40 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const FORMINPUT_KEY = "feedback-form-state";
+const parseJson = JSON.parse(localStorage.getItem(FORMINPUT_KEY));
+
+let formData = {...parseJson};
+const form = document.querySelector(".feedback-form");
+populateTexttareaInput();
+
+form.addEventListener("submit", onFormSubmit);
+form.addEventListener("input", throttle(onForm, 500));
+
+function onFormSubmit(e) {
+    e.preventDefault();
+const formDataForBackend = new FormData(e.currentTarget)
+formDataForBackend.forEach((name, value) => {
+    console.log(value, name);
+})
+    localStorage.removeItem(FORMINPUT_KEY);
+    e.currentTarget.reset();
+}
+
+function onForm(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(FORMINPUT_KEY, JSON.stringify(formData));
+    }
+
+function populateTexttareaInput() {
+    const savedForm = parseJson;
+    if (savedForm) {
+    Object.entries(savedForm).forEach(([name, value]) => {
+    form.elements[name].value = value
+    })
+    }
+}
