fa215946d6af450f2adc2349f995bc61eba55c61
Alina Proskurnitska <proskur1987@gmail.com>
2022-08-21 11:34:50 +0300

    fix-fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('#vimeo-player');
-const player = new Player(iframe, { muted: true, autoplay: true });
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
 player.on('timeupdate', throttle(function getTime (e) {
@@ -10,14 +10,11 @@ player.on('timeupdate', throttle(function getTime (e) {
 
     localStorage.setItem(LOCALSTORAGE_KEY, currentSecond);
 }), 1000);
-    
-const timeLocalStorage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-
-player.setCurrentTime(timeLocalStorage);
 
 const savedTime = localStorage.getItem(LOCALSTORAGE_KEY);
 if (savedTime) {
-    const timeFromLocalStor = JSON.parse(savedTime);
+    const timeLocalStorage = JSON.parse(savedTime);
 
-    player.setCurrentTime(timeFromLocalStor);
-}
+player.setCurrentTime(timeLocalStorage);
+};
+
